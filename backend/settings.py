7dae04ada97e97cf7a29adb00da6f3ea59de6dd6
quiland15 <quiland86@gmail.com>
2025-07-21 09:18:26 +0800

    make cj file

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sg^6+gp&^gp(u+dr)$@rxz%)=(t1nw$luc^qk$1=s)k^g@lb)h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

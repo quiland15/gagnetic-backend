e367c31bf12aff1da29213387596f4a79e15f173
quiland15 <quiland86@gmail.com>
2025-07-21 13:29:12 +0800

    change

--- product/cjApi.py
+++ product/cjApi.py
@@ -7,6 +7,7 @@ def fetch_cj_products(page_num=1, keyword=""):
         "keyword": keyword,
         "pageSize": 10,
         "sort": "new",
+        "productType" : "phone accessories"
     }
 
     headers = {

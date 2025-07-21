51b1b055b92adcfa2a2ca3a7743ddf1383fac872
quiland15 <quiland86@gmail.com>
2025-07-21 09:17:31 +0800

    make cj file

--- product/views.py
+++ product/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .cj_api import fetch_cj_products
+from .cjApi import fetch_cj_products
 
 class ProductListAPIView(APIView):
     def get(self, request):

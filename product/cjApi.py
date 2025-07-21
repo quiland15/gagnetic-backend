c2c437a31fb0ce4c9c43613b1f30862d6ef33b84
quiland15 <quiland86@gmail.com>
2025-07-21 13:03:09 +0800

    change

--- product/cjApi.py
+++ product/cjApi.py
@@ -1,18 +1,23 @@
 import requests
 
-def fetch_cj_products(page_num=1, page_size=10):
-    url = 'https://developers.cjdropshipping.com/api2.0/v1/product/list'
-    headers = {
-        'CJ-Access-Token': '6d0704928897419eba07bf66316aee3e',  # ganti dengan token asli
-        'Content-Type': 'application/json',
-    }
-    body = {
+def fetch_cj_products(page_num=1, keyword=""):
+    url = f"https://api.cjdropshipping.com/api/product/search"
+    payload = {
         "pageNum": page_num,
-        "pageSize": page_size
+        "keyword": keyword,
+        "pageSize": 10,
+        "sort": "new",
     }
-    response = requests.post(url, json=body, headers=headers)
 
-    if response.status_code == 200:
-        return response.json().get('data', {}).get('list', [])
+    headers = {
+        "CJ-Access-Token": "6d0704928897419eba07bf66316aee3e",
+        "Content-Type": "application/json",
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    data = response.json()
+
+    if data.get("code") == 200:
+        return data.get("data", {}).get("list", [])
     else:
         return []

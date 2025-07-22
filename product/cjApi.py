c292252b13690f2a201fc941e921f6a223a216fb
quiland15 <quiland86@gmail.com>
2025-07-22 10:52:45 +0800

    just test the api

--- product/cjApi.py
+++ product/cjApi.py
@@ -4,19 +4,13 @@ import time
 def fetch_cj_products(page_num=1):
     time.sleep(1)  # Hindari QPS limit
 
-    url = "https://developers.cjdropshipping.com/api2.0/v1/product/myProduct/query"
-
-    params = {
-        "pageNum": page_num,
-        "pageSize": 10,
-        "language": "en"
-    }
+    url = f"https://developers.cjdropshipping.com/api2.0/v1/product/myProduct/query?pageNum={page_num}&pageSize=10&language=en"
 
     headers = {
-        "CJ-Access-Token": "API@CJ4528776@CJ:eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIyNjAxNCIsInR5cGUiOiJBQ0NFU1NfVE9LRU4iLCJzdWIiOiJicUxvYnFRMGxtTm55UXB4UFdMWnlyVEpvYzFGeW9SeE0yVmg4T2lEZmZvZ2RSTTZIZGVUd25tdTZJc1Y5WnF6NU5YRUszWjA4KzFWanBoSGI5cUNuenlNTHJLWG80UVI5M2Y2cFNGcjJvbUEwbGsxc1Q5SGpiajVhRlZhR1psRUczdjltZjBKYzcxd3V6cDMzTmxpYjE5RlpOeXlyY0FIQSs5ak9MSGllMDdHZkJxTENFSkRTWFJvT0VmSDh3TjlOSFYrRTkyMXJhd2FmQXJadVMyTUIwNmtrVllsZjlsb2xpR2M4RHlkQSs4QS8rcGZmbm4wNW0zR204bW1VZ0lQRndCNGFqQW1hdEZwWWdTUWtkN0ozK2ZvU0hDekVPTEg1dmNYd0Y1YXZJST0iLCJpYXQiOjE3NTMwNzUzNTd9.EBAiuPa5-8g6yFgKtYhIDD-1s1ehuDNcB0mLaBeQSUI",  # Token kamu
+        "CJ-Access-Token": "API@CJ4528776@CJ:...",  # Token kamu
     }
 
-    response = requests.post(url, json=params, headers=headers)
+    response = requests.get(url, headers=headers)
 
     print("MY PRODUCTS STATUS:", response.status_code)
     print("MY PRODUCTS RAW:", response.text)

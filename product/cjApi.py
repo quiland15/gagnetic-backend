fa092772ca311b449c98d5f736220593b2087f54
quiland15 <quiland86@gmail.com>
2025-07-22 10:40:51 +0800

    just test the api

--- product/cjApi.py
+++ product/cjApi.py
@@ -4,7 +4,7 @@ import time
 def fetch_cj_products(page_num=1):
     time.sleep(1)  # Hindari QPS limit
 
-    url = "https://developers.cjdropshipping.com/api2.0/v1/product/storehouse/productList"
+    url = "https://developers.cjdropshipping.com/api2.0/v1/product/storehouse/myproducts"
 
     params = {
         "pageNum": page_num,

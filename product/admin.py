ed2c96b8eb02a60459667eb6b9782c6f3775af12
quiland15 <quiland86@gmail.com>
2025-07-29 15:45:49 +0800

    change

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name_en', 'sku', 'sell_price', 'your_price', 'stock', 'factory_stock', 'warehouse_name')
+    search_fields = ('name_en', 'sku')
--- product/models.py
+++ product/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    cj_id = models.CharField(max_length=100, unique=True)  # ID unik dari CJ
+    name_en = models.CharField(max_length=255)
+    sku = models.CharField(max_length=100)
+    
+    big_image = models.URLField(max_length=500)
+    
+    sell_price = models.DecimalField(max_digits=10, decimal_places=2)
+    your_price = models.DecimalField(max_digits=10, decimal_places=2)
+    
+    stock = models.IntegerField(default=0)  # Stok CJ
+    factory_stock = models.IntegerField(default=0)  # Stok pabrik
+    warehouse_name = models.CharField(max_length=100)
+    
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name_en} - {self.sku}"

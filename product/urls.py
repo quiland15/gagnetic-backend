10b93acfc908cf81521736bca73f6f3b3507ff55
quiland15 <quiland86@gmail.com>
2025-07-31 18:36:40 +0800

    change

--- product/urls.py
+++ product/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 # from .views import cj_product_list_view
 from .views import my_products_view
 from .views import sync_cj_products
+from .views import my_products_api
 
 urlpatterns = [
     # path('products/', ProductListAPIView.as_view(), name='product-list'),
     # path('cj-products/', cj_product_list_view, name='cj_product_list'),
     path('my-products/', my_products_view, name='my_product_list'),
     path('sync-cj-products/', sync_cj_products, name='sync-cj-products'),
+    path('my-products-api/', my_products_api, name='my_products_api'),
 ]
--- product/views.py
+++ product/views.py
@@ -7,6 +7,35 @@ import json
 from django.core.paginator import Paginator
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_apscheduler.jobstores import DjangoJobStore
+from django.core import serializers
+from django.http import JsonResponse
+
+def my_products_api(request):
+    page = int(request.GET.get('page', 1))
+    product_list = Product.objects.all().order_by('-last_updated')
+    paginator = Paginator(product_list, 12)
+    products = paginator.get_page(page)
+
+    data = []
+    for product in products:
+        data.append({
+            'id': product.id,
+            'name_en': product.name_en,
+            'sku': product.sku,
+            'big_image': product.big_image,
+            'sell_price': float(product.sell_price),
+            'your_price': float(product.your_price),
+            'stock': product.stock,
+            'factory_stock': product.factory_stock,
+            'warehouse_name': product.warehouse_name,
+        })
+
+    return JsonResponse({
+        'page': page,
+        'total_pages': paginator.num_pages,
+        'total_items': paginator.count,
+        'products': data,
+    })
 
 def my_products_view(request):
     page = int(request.GET.get('page', 1))
